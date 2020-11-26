707cb55bf3283129d41a92c5c7a77b8e69b9581e
choieunji <62586967+choieunii@users.noreply.github.com>
2020-11-26 14:36:19 +0900

    백준 1436번 영화감독 숌

---
+++ baekjoon1436.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class baekjoon1436 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s= new Scanner(System.in);
+		int n = s.nextInt();
+		int count=666;
+		int num = 0;
+		String strcnt=""; 
+		String answer = "";
+		while(n!=num) {
+			strcnt=Integer.toString(count);
+			if(strcnt.contains("666"))
+			{
+				answer=strcnt;
+				num++;
+			}
+			count++;
+		}
+		System.out.println(answer);
+	}
+}
