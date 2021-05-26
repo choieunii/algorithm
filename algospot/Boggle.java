4cc54a1342e5d0a841f4dd0c0069bf081f23d149
choieunii <ghk784@dgu.ac.kr>
2021-05-27 02:31:54 +0900

    feat : 알고스팟 Boggle

--- algospot/Boggle.java
+++ algospot/Boggle.java
@@ -35,26 +35,19 @@ public class Boggle {
             bw.close();
         }
     }
-    // 5 x 5 보글 게임 판의 해당 위치에서 주어진 단어가 시작하는지를 반환.
     static boolean hasWord(int y, int x, String word) {
-        // 1. 시작 위치가 범위 밖이면 무조건 실패
         if (!(y >= 0 && y < 5 && x >= 0 && x < 5)) {
             return false;
         }
-        // 2. 첫 글자가 일치하지 않으면 실패
         if (map[y][x] != word.charAt(0)) {
             return false;
         }
-        // 3. 단어가 일치하면서 원하는 단어가 한 글자인 경우 항상 성공
         if (word.length() == 1) {
             return true;
         }
-        // 인접한 여덟 칸을 검사한다.
         for (int direction = 0; direction < 8; direction++) {
             int nextY = y + dy[direction];
             int nextX = x + dx[direction];
-            // word.substring(1)을 하면
-            // 첫 번째 index 0을 제외하고 [1...]을 계속해서 수행한다.
             if (hasWord(nextY, nextX, word.substring(1))) {
                 return true;
             }
